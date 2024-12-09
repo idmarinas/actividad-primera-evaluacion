11b7f084ca7ded2ae90b1ce250fd5fa5436977e2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-09 16:58:01 +0100

    Se agrega configuración para el destino de la build y la base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/actividad-primera-evaluacion/',
+	build: {
+		outDir: 'docs/'
+	},
   plugins: [
     vue(),
     vueDevTools(),
