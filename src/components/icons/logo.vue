c85955e2b8fa84d6fcc39441d0105559b9d4632c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-12 22:27:30 +0100

    Se reemplaza el logo por un SVG incrustado

---
+++ src/components/icons/logo.vue
@@ -0,0 +1,3 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" class="bg-gold-sand-300 text-gold-sand-900 p-1 rounded-lg" viewBox="0 0 24 24"><title>Logo bonito de dos árboles que representan a la Carpinteria MaderAvilés</title><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"><path d="M10 10v.2A3 3 0 0 1 8.9 16H5a3 3 0 0 1-1-5.8V10a3 3 0 0 1 6 0m-3 6v6m6-3v3"/><path d="M12 19h8.3a1 1 0 0 0 .7-1.7L18 14h.3a1 1 0 0 0 .7-1.7L16 9h.2a1 1 0 0 0 .8-1.7L13 3l-1.4 1.5"/></g></svg>
+</template>
