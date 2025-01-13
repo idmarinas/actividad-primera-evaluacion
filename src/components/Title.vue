41197e0c63e40a2fbbabcf55c4022c57b885525e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-13 21:22:18 +0100

    Primera anotación

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { usePageTitleStore } from '@/stores/page-title'
+import { storeToRefs } from 'pinia';
+import { computed, watch } from 'vue';
+
+const store = usePageTitleStore()
+const { title } = storeToRefs(store)
+
+watch(title, title => {
+	document.title = `${title} - MaderAvilés Carpintería`
+})
+</script>
+
+<template>
+	<h1 v-show="title !== null">{{ title }}</h1>
+</template>
