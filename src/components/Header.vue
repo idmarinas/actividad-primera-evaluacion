0f11f0309a926bf4c3d83c683f5511e277205155
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-14 18:38:34 +0100

    Se mejora el diseño

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,23 +9,23 @@ import Portada from '@/components/icons/portada.vue'
 <template>
 	<header class="p-4 px-7 bg-gold-sand-700 text-gold-sand-50 border-b-gold-sand-700 border-b shadow-md">
 		<div class="container mx-auto flex flex-row gap-5">
-			<Logo width="36" />
-			<nav class="w-full">
+			<Logo width="36" class="flex-initial" />
+			<nav class="flex-1">
 				<ul class="flex flex-row gap-7">
 					<li>
 						<RouterLink class="flex gap-2 items-center px-2 border-t-4 border-b" :to="{ name: 'home' }">
-							<Portada width="32" /> Portada
+							<Portada width="32" /> <span class="hidden md:block">Portada</span>
 						</RouterLink>
 					</li>
 					<li class="flex-1"></li>
 					<li>
 						<RouterLink class="flex gap-2 items-center px-2 border-t-4 border-b" :to="{ name: 'pieces_list' }">
-							<Pieza width="32" /> Piezas
+							<Pieza width="32" /> <span class="hidden md:block">Piezas</span>
 						</RouterLink>
 					</li>
 					<li>
 						<RouterLink class="flex gap-2 items-center px-2 border-t-4 border-b" :to="{ name: 'order_list' }">
-							<Pedidos width="32" /> Pedidos
+							<Pedidos width="32" /> <span class="hidden md:block">Pedidos</span>
 						</RouterLink>
 					</li>
 				</ul>
