7f5ed33736774ec5f9197fd4b4133fbfcfce488b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-15 14:56:44 +0100

    Primera anotación

---
+++ src/components/forms/buttons/icons/info.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from '../../button.vue'
+import Info from '@/components/icons/info.vue'
+
+</script>
+
+<template>
+	<Button button-style="info" :circle="true"><Info width="24" /><span class="sr-only">Información</span></Button>
+</template>
