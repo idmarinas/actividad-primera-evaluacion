ed8b457697538bb9585e0c5f6a3b4a38c302caec
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-15 12:38:56 +0100

    Primera anotación

---
+++ src/components/forms/buttons/icons/del.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Button from '../../button.vue'
+import Del from '@/components/icons/trash.vue'
+</script>
+
+<template>
+	<Button button-style="danger" :circle="true"><Del width="24" /><span class="sr-only">Borrar</span></Button>
+</template>
---
+++ src/components/forms/buttons/icons/edit.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from '../../button.vue'
+import Edit from '@/components/icons/edit.vue'
+
+</script>
+
+<template>
+	<Button button-style="yellow" :circle="true"><Edit width="24" /><span class="sr-only">Editar</span></Button>
+</template>
