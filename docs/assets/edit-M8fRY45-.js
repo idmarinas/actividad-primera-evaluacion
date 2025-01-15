d88e0fec4bc4b19d4a795df98356b082297c8def
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-15 23:59:51 +0100

    Actualizado

---
+++ docs/assets/edit-M8fRY45-.js
@@ -0,0 +1,2 @@
+import{o as s,a as r,w as a,b as e,d as o,_ as n}from"./index-B1bQJuSh.js";import{T as c,E as i}from"./edit-BewcTDo-.js";const u={__name:"del",setup(_){return(l,t)=>(s(),r(n,{"button-style":"danger",circle:!0},{default:a(()=>[e(c,{width:"24"}),t[0]||(t[0]=o("span",{class:"sr-only"},"Borrar",-1))]),_:1}))}},m={__name:"edit",setup(_){return(l,t)=>(s(),r(n,{"button-style":"yellow",circle:!0},{default:a(()=>[e(i,{width:"24"}),t[0]||(t[0]=o("span",{class:"sr-only"},"Editar",-1))]),_:1}))}};export{m as _,u as a};
+//# sourceMappingURL=edit-M8fRY45-.js.map
--- docs/assets/home-DMQAqSBN.js
+++
@@ -1,2 +0,0 @@
-import{u as o,s as a,c as s,o as r}from"./index-DChD_NpM.js";const _={__name:"home",setup(c){const e=o(),{title:t}=a(e);return t.value="Portada",(l,n)=>(r(),s("article",null," Portada "))}};export{_ as default};
-//# sourceMappingURL=home-DMQAqSBN.js.map
