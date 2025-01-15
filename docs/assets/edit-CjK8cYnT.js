895e206120d651dd20fc4b03086ef3e3b59ef4f3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-15 23:07:48 +0100

    Actualizado

--- docs/assets/edit-CjK8cYnT.js
+++
@@ -1,2 +0,0 @@
-import{o as s,a as r,w as a,b as e,d as o,_ as n}from"./index-Cg5nVZOc.js";import{T as c,E as i}from"./edit-BZ9c6_rY.js";const u={__name:"del",setup(_){return(l,t)=>(s(),r(n,{"button-style":"danger",circle:!0},{default:a(()=>[e(c,{width:"24"}),t[0]||(t[0]=o("span",{class:"sr-only"},"Borrar",-1))]),_:1}))}},m={__name:"edit",setup(_){return(l,t)=>(s(),r(n,{"button-style":"yellow",circle:!0},{default:a(()=>[e(i,{width:"24"}),t[0]||(t[0]=o("span",{class:"sr-only"},"Editar",-1))]),_:1}))}};export{m as _,u as a};
-//# sourceMappingURL=edit-CjK8cYnT.js.map
