8049a316465f74ea19311c47f7e220a98505a4ac
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-16 12:50:50 +0100

    Actualizado

---
+++ docs/assets/home-DVk8NnpG.js
@@ -0,0 +1,2 @@
+import{u as o,s as a,c as s,o as r}from"./index-DHnoSDYb.js";const _={__name:"home",setup(c){const e=o(),{title:t}=a(e);return t.value="Portada",(l,n)=>(r(),s("article",null," Portada "))}};export{_ as default};
+//# sourceMappingURL=home-DVk8NnpG.js.map
