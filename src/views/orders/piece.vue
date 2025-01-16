64baf0bc087900a7d168f9223462eaecfdf02ccd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-16 12:50:01 +0100

    Se informa sobre los errores

--- src/views/orders/piece.vue
+++ src/views/orders/piece.vue
@@ -1,14 +1,12 @@
 <script setup>
 import Input from '@/components/forms/input.vue'
 import Button from '@/components/forms/button.vue'
+import Alert from '@/components/dialogs/alert.vue'
+
 import { usePiecesStore } from '@/stores/pieces'
+import { computed } from 'vue'
 
 const store = usePiecesStore()
-
-// const model = defineModel({
-// 	bothFaces,
-// 	cut
-// })
 
 const props = defineProps({
 	isEdit: Boolean
@@ -20,19 +18,29 @@ const dimensionsLength = defineModel('dimensionsLength')
 const dimensionsWidth = defineModel('dimensionsWidth')
 const dimensionsThickness = defineModel('dimensionsThickness')
 const color = defineModel('color')
-const bothFaces = defineModel('bothFaces', {
-	type: Boolean,
-	set (value) {
-		value = Boolean(value)
-		return value
+const bothFaces = defineModel('bothFaces', { type: Boolean, set: (value) => Boolean(value) })
+const cut = defineModel('cut', { type: Boolean, set: (value) => Boolean(value) })
+
+const errors = defineModel('errors')
+
+const isValid = computed(() => {
+	let valid = true
+
+	for(let key in errors.value) {
+		if (key === 'dimensions') {
+			for (let k in errors.value[key]) {
+				if (errors.value[key][k].length > 0) {
+					valid = false
+				}
+			}
+		} else {
+			if (errors.value[key].length > 0) {
+				valid = false
+			}
+		}
 	}
-})
-const cut = defineModel('cut', {
-	type: Boolean,
-	set (value) {
-		value = Boolean(value)
-		return value
-	}
+
+	return valid
 })
 
 function generatePieceId() {
@@ -41,13 +49,22 @@ function generatePieceId() {
 </script>
 
 <template>
+	<Alert type="error" class="col-span-full" v-if="!isValid">
+		<template #title>
+			La información de la pieza contiene errores
+		</template>
+		<div v-for="messages in errors" class=" space-y-1">
+			<template v-for="msg in messages"><div>{{ typeof msg === 'string' ? msg : msg[0] }}</div></template>
+		</div>
+	</Alert>
+
 	<input type="hidden" v-model="order">
 	<div class="col-span-2 flex gap-3 items-center">
 		<label for="piece_id">Nº de pedido</label>
-		<Input v-if="props.isEdit" type="number" readonly class="rounded-lg flex-1" id="piece_id" v-model="id" />
+		<Input v-if="props.isEdit" :error="errors.id.length > 0" type="number" readonly class="rounded-lg flex-1" id="piece_id" v-model="id" />
 		<template v-else>
 			<div class="flex-1 flex gap-0">
-				<Input type="number" readonly class="rounded-l-lg flex-1" id="piece_id" v-model="id" />
+				<Input :error="errors.id.length > 0" type="number" readonly class="rounded-l-lg flex-1" id="piece_id" v-model="id" />
 				<Button class="rounded-r-lg" @click="generatePieceId()" :disabled="id !== undefined">Generar</Button>
 			</div>
 		</template>
@@ -58,22 +75,22 @@ function generatePieceId() {
 		<div class="col-span-full grid grid-cols-3 gap-3">
 			<div class="flex gap-3 items-center">
 				<label for="piece_dimensions_length">Largo (cm)</label>
-				<Input class="rounded-lg flex-1" type="number" id="piece_dimensions_length" v-model="dimensionsLength" step="1" />
+				<Input :error="errors.dimensions.length.length > 0" class="rounded-lg flex-1" type="number" id="piece_dimensions_length" v-model="dimensionsLength" step="1" />
 			</div>
 			<div class="flex gap-3 items-center">
 				<label for="piece_dimensions_width">Ancho (cm)</label>
-				<Input class="rounded-lg flex-1" type="number" id="piece_dimensions_width"  v-model="dimensionsWidth" step="1" />
+				<Input :error="errors.dimensions.width.length > 0" class="rounded-lg flex-1" type="number" id="piece_dimensions_width"  v-model="dimensionsWidth" step="1" />
 			</div>
 			<div class="flex gap-3 items-center">
 				<label for="piece_dimensions_thickness">Grosor (cm)</label>
-				<Input class="rounded-lg flex-1" type="number" id="piece_dimensions_thickness"  v-model="dimensionsThickness" step="1" />
+				<Input :error="errors.dimensions.thickness.length > 0" class="rounded-lg flex-1" type="number" id="piece_dimensions_thickness"  v-model="dimensionsThickness" step="1" />
 			</div>
 		</div>
 	</div>
 
 	<div class="flex gap-3 items-center">
 		<label for="piece_color">Color chapado</label>
-		<Input class="rounded-lg flex-1" type="text" id="piece_color" v-model="color" />
+		<Input :error="errors.color.length > 0" class="rounded-lg flex-1" type="text" id="piece_color" v-model="color" />
 	</div>
 	
 	<div class="flex gap-3 items-center">
